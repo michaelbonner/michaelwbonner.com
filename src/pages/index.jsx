409c7c72b2fe3619fe295df59d6688249808d792
Michael Bonner <michaelwbonner@gmail.com>
2020-08-10 17:42:11 -0600

    update insta handle

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,21 @@
 /* global tw */
-import React from 'react';
-import styled from 'react-emotion';
-import 'typeface-cantata-one';
-import 'typeface-open-sans';
-import { Parallax, ParallaxLayer } from 'react-spring/dist/addons';
-import SEO from '../components/SEO';
-import ProjectCard from '../components/ProjectCard';
-import avatar from '../images/avatar.jpg';
-import '../styles/global';
+import React from "react";
+import styled from "react-emotion";
+import "typeface-cantata-one";
+import "typeface-open-sans";
+import { Parallax, ParallaxLayer } from "react-spring/dist/addons";
+import SEO from "../components/SEO";
+import ProjectCard from "../components/ProjectCard";
+import avatar from "../images/avatar.jpg";
+import "../styles/global";
 
 const Divider = styled(ParallaxLayer)`
-  ${tw('absolute w-full h-full')};
-  background: ${props => props.bg};
+  ${tw("absolute w-full h-full")};
+  background: ${(props) => props.bg};
   svg {
-    fill: ${props => props.fill};
+    fill: ${(props) => props.fill};
   }
-  clip-path: ${props => props.clipPath};
+  clip-path: ${(props) => props.clipPath};
 `;
 
 const DividerMiddle = styled(Divider)`
@@ -23,34 +23,36 @@ const DividerMiddle = styled(Divider)`
 `;
 
 const Content = styled(ParallaxLayer)`
-  ${tw('p-6 md:p-12 lg:p-24 justify-center items-center flex z-50')};
+  ${tw("p-6 md:p-12 lg:p-24 justify-center items-center flex z-50")};
 `;
 
 const Hero = styled.div`
-  ${tw('w-full xl:w-2/3')};
+  ${tw("w-full xl:w-2/3")};
 `;
 
 const Inner = styled.div`
-  ${tw('w-full xxl:w-2/3 text-center lg:text-left')};
+  ${tw("w-full xxl:w-2/3 text-center lg:text-left")};
 `;
 
 const BigTitle = styled.h1`
-  ${tw('text-5xl lg:text-6xl font-serif text-white mb-6 tracking-wide')};
+  ${tw("text-5xl lg:text-6xl font-serif text-white mb-6 tracking-wide")};
   text-shadow: 0 5px 35px rgba(255, 255, 255, 0.15);
 `;
 
 const Title = styled.h1`
-  ${tw('text-4xl lg:text-4xl font-serif text-white mb-8 tracking-wide relative inline-block')};
+  ${tw(
+    "text-4xl lg:text-4xl font-serif text-white mb-8 tracking-wide relative inline-block"
+  )};
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
 `;
 
 const Subtitle = styled.p`
-  ${tw('text-2xl lg:text-4xl font-sans text-white mt-8 xxl:w-3/4')};
+  ${tw("text-2xl lg:text-4xl font-sans text-white mt-8 xxl:w-3/4")};
   text-shadow: 0 2px 15px rgba(0, 0, 0, 0.2);
 `;
 
 const ProjectsWrapper = styled.div`
-  ${tw('flex flex-wrap justify-between mt-8')};
+  ${tw("flex flex-wrap justify-between mt-8")};
   display: grid;
   grid-gap: 4rem;
   grid-template-columns: repeat(2, 1fr);
@@ -64,12 +66,12 @@ const ProjectsWrapper = styled.div`
 `;
 
 const WaveWrapper = styled.div`
-  ${tw('absolute pin-b w-full')};
+  ${tw("absolute pin-b w-full")};
   transform: matrix(1, 0, 0, -1, 0, 0);
 `;
 
 const InnerWave = styled.div`
-  ${tw('relative h-full')};
+  ${tw("relative h-full")};
   svg {
     width: 100%;
     height: 40vh;
@@ -77,23 +79,25 @@ const InnerWave = styled.div`
 `;
 
 const AboutHero = styled.div`
-  ${tw('flex flex-col lg:flex-row items-center mt-8')};
+  ${tw("flex flex-col lg:flex-row items-center mt-8")};
 `;
 
 const Avatar = styled.img`
-  ${tw('rounded-full w-32 xl:w-48 shadow-lg h-auto')};
+  ${tw("rounded-full w-32 xl:w-48 shadow-lg h-auto")};
 `;
 
 const AboutSub = styled.span`
-  ${tw('text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl')};
+  ${tw("text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl")};
 `;
 
 const AboutDesc = styled.p`
-  ${tw('text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12')};
+  ${tw(
+    "text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12"
+  )};
 `;
 
 const ContactText = styled.p`
-  ${tw('text-grey-light font-sans text-xl md:text-2xl lg:text-3xl')};
+  ${tw("text-grey-light font-sans text-xl md:text-2xl lg:text-3xl")};
   a {
     color: #e07628;
     text-decoration: none;
@@ -101,7 +105,9 @@ const ContactText = styled.p`
 `;
 
 const Footer = styled.footer`
-  ${tw('text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg')};
+  ${tw(
+    "text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg"
+  )};
   a {
     color: #e07628;
     text-decoration: none;
@@ -134,61 +140,75 @@ const Index = () => (
               link="https://smackgrc.com"
               bg="linear-gradient(to right, #ffb81c 0%, #F0AE1B 100%)"
             >
-              My friend Jeff Moore built some awesome software to help businesses manage their GRC needs. Go check it
-              out.
+              My friend Jeff Moore built some awesome software to help
+              businesses manage their GRC needs. Go check it out.
             </ProjectCard>
             <ProjectCard
               title="Wasatch Covers"
               link="https://wasatchcovers.com"
               bg="linear-gradient(to right, #5AB948 0%, #5AB948 100%)"
             >
-              Need a window well cover in Utah? Go buy a custom one from Wasatch Covers
+              Need a window well cover in Utah? Go buy a custom one from Wasatch
+              Covers
             </ProjectCard>
             <ProjectCard
               title="Quiznado"
               link="https://quiznado.com/"
               bg="linear-gradient(to right, #3EA1AD 0%, #51D1E1 100%)"
             >
-              A quiz game for parents to incentivize kids to learn. Built using Angular, Ionic and Laravel.
+              A quiz game for parents to incentivize kids to learn. Built using
+              Angular, Ionic and Laravel.
             </ProjectCard>
             <ProjectCard
               title="Legit Local"
               link="https://legitlocal.com"
               bg="linear-gradient(to right, #F4A637 0%, #F8A927 100%)"
             >
-              A SaaS application to help local businesses to connect with their customers. Built with Laravel and React.
+              A SaaS application to help local businesses to connect with their
+              customers. Built with Laravel and React.
             </ProjectCard>
             <ProjectCard
               title="Redirects Wizard"
               link="https://github.com/michaelbonner/redirects-wizard"
               bg="linear-gradient(to right, #009FFF 0%, #ec2F4B 100%)"
             >
-              A tool to help find and address 404s, even before the site goes live. Built with Laravel and Vue.
+              A tool to help find and address 404s, even before the site goes
+              live. Built with Laravel and Vue.
             </ProjectCard>
             <ProjectCard
               title="Is It Down Checker"
               link="https://github.com/michaelbonner/is-it-down"
               bg="linear-gradient(to right, #b91d73 0%, #f953c6 100%)"
             >
-              An app to keep track of which sites are up, and the SSL status of each. Built with Laravel.
+              An app to keep track of which sites are up, and the SSL status of
+              each. Built with Laravel.
             </ProjectCard>
           </ProjectsWrapper>
         </Inner>
       </Content>
       <Divider speed={0.1} offset={1} factor={2} />
-      <Divider bg="#23262b" clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)" speed={0.2} offset={3} />
+      <Divider
+        bg="#23262b"
+        clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)"
+        speed={0.2}
+        offset={3}
+      />
       <Divider speed={0.1} offset={3} />
       <Content speed={0.4} offset={3}>
         <Inner>
           <Title>About Me</Title>
           <AboutHero>
             <Avatar src={avatar} alt="Michael Bonner" />
-            <AboutSub>A web and software developer based in Salt Lake, UT.</AboutSub>
+            <AboutSub>
+              A web and software developer based in Salt Lake, UT.
+            </AboutSub>
           </AboutHero>
           <AboutDesc>
-            Over 15 years of development experience, and 5 years of director level agency experience. I know what works,
-            what doesn't work, and why. I can use that knowledge to build you a site, mobile app, or software that works
-            for your needs. I'll sit with you and your team, we'll make a plan, and we'll build some awesome stuff.
+            Over 15 years of development experience, and 5 years of director
+            level agency experience. I know what works, what doesn't work, and
+            why. I can use that knowledge to build you a site, mobile app, or
+            software that works for your needs. I'll sit with you and your team,
+            we'll make a plan, and we'll build some awesome stuff.
           </AboutDesc>
         </Inner>
       </Content>
@@ -201,18 +221,22 @@ const Index = () => (
         <Inner>
           <Title>Get in touch</Title>
           <ContactText>
-            Say{' '}
+            Say{" "}
             <a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#109;&#105;&#099;&#104;&#097;&#101;&#108;&#119;&#098;&#111;&#110;&#110;&#101;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">
               Hi
-            </a>{' '}
-            or find me on other platforms: <a href="https://www.linkedin.com/in/michaelbonner/">LinkedIn</a> &{' '}
-            <a href="https://www.instagram.com/mbonner4">Instagram</a>
+            </a>{" "}
+            or find me on other platforms:{" "}
+            <a href="https://www.linkedin.com/in/michaelbonner/">LinkedIn</a> &{" "}
+            <a href="https://www.instagram.com/michael__bonner">Instagram</a>
           </ContactText>
         </Inner>
         <Footer>
           <small>&copy;</small>
-          {new Date().getFullYear()} by Michael Bonner.{' '}
-          <a href="https://github.com/michaelbonner/michaelwbonner.com">Github</a>.
+          {new Date().getFullYear()} by Michael Bonner.{" "}
+          <a href="https://github.com/michaelbonner/michaelwbonner.com">
+            Github
+          </a>
+          .
         </Footer>
       </Content>
       <Divider speed={0.1} offset={4} />
